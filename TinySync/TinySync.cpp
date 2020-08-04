bda75e879335413e37a60aba13085defcead8da6
Sergey Firsov <fearthunder@yandex.ru>
2020-08-04 14:15:05 +0300

    Replcae Controller to IController in main

--- TinySync/TinySync.cpp
+++ TinySync/TinySync.cpp
@@ -1,13 +1,14 @@
 #include <memory>
 
 #include "Controller/Controller.h"
+#include "Controller/IController.h"
 #include "InputManager/InputManager.h"
 
 int main(int argc, char* argv[])
 {
     std::shared_ptr<InputManager> Input_manager{new InputManager()};
 
-    std::shared_ptr<Controller> Sync_controller{new Controller(Input_manager)};
+    std::shared_ptr<IController> Sync_controller{new Controller(Input_manager)};
 
     Sync_controller->InputConsoleArgument(argc, argv);
 
