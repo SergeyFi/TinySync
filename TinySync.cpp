2fc60b6b446fa541f076f5bb51703678820e887b
Sergey Firsov <fearthunder@yandex.ru>
2020-08-18 20:53:23 +0300

    Add commands

--- TinySync.cpp
+++ TinySync.cpp
@@ -4,7 +4,11 @@
 #include "InputManager/InputManager.h"
 #include "OutputManager/OutputManager.h"
 #include "SyncManager/SyncManager.h"
-
+#include "Commands/CommandAddOrigin.h"
+#include "Commands/CommandAddTarget.h"
+#include "Commands/CommandSync.h"
+#include "Commands/CommandBalance.h"
+#include "Commands/CommandCleanTarget.h"
 
 int main(int argc, char* argv[])
 {
@@ -14,7 +18,16 @@ int main(int argc, char* argv[])
 
     std::shared_ptr<ISync> Sync_manager {new SyncManager()};
 
-    std::shared_ptr<IController> Sync_controller{new Controller(true ,Input_manager, Output_manager, Sync_manager)};
+    // Set commands
+    std::map<CommandType, std::shared_ptr<ICommand>> commands;
+    commands.insert(std::make_pair(CommandType::origin, new CommandAddOrigin(CommandType::origin)));
+    commands.insert(std::make_pair(CommandType::target, new CommandAddTarget(CommandType::target)));
+    commands.insert(std::make_pair(CommandType::sync, new CommandSync(CommandType::sync)));
+    commands.insert(std::make_pair(CommandType::balance, new CommandBalance(CommandType::balance)));
+    commands.insert(std::make_pair(CommandType::clean, new CommandCleanTarget(CommandType::clean)));
+
+    std::shared_ptr<IController> Sync_controller
+    {new Controller(true ,Input_manager, Output_manager, Sync_manager, commands)};
 
     Input_manager->AddController(Sync_controller);
 
