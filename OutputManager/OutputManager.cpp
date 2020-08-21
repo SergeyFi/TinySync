f3a2647bcc3650dd750774b7df95063a59c6317d
Sergey Firsov <fearthunder@yandex.ru>
2020-08-22 00:32:42 +0300

    Update help info

--- OutputManager/OutputManager.cpp
+++ OutputManager/OutputManager.cpp
@@ -8,6 +8,7 @@ void OutputManager::PrintHelp()
     std::cout << "Set path to origin: -O your_path or --origin your_path" << std::endl;
     std::cout << "Set path to target: -T your_path or -- target your_path" << std::endl;
     std::cout << "Sync: -S or --sync" << std::endl;
+    std::cout << "Sync with update old files: -SU or --syncUpdate" << std::endl;
     std::cout << "Balance: -B or --balance" << std::endl;
     std::cout << "Clean: -C or --clean" << std::endl;
     std::cout << "Current version: -V or --version" << std::endl;
