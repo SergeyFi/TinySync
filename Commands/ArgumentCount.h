03c670b73e09ba397d026b0350872944d07915e6
Sergey Firsov <fearthunder@yandex.ru>
2020-08-25 14:27:32 +0300

    Add argument count and priority structs

---
+++ Commands/ArgumentCount.h
@@ -0,0 +1,11 @@
+#pragma once
+
+struct ArgCount
+{
+    unsigned int count;
+
+    ArgCount(unsigned int argumentCount)
+    {
+        count = argumentCount;
+    }
+};
---
+++ Commands/Priority.h
@@ -0,0 +1,11 @@
+#pragma once
+
+struct Priority
+{
+    unsigned int priority;
+
+    Priority(unsigned int newPriority)
+    {
+        priority = newPriority;
+    }
+};
